3e0cd7af7e4d601592e98e2af6563a22c75fec23
Khanh Dang <khanh.2dbt@gmail.com>
2021-10-08 18:55:09 +0300

    Add tuni test for link station list model

--- server/src/models/test/calculation.spec.ts
+++ server/src/models/test/calculation.spec.ts
@@ -1,9 +1,10 @@
 import { LinkStation } from '../LinkStation';
+import { LinkStationList } from '../LinkStationList';
 import { Point } from '../Point';
 
 describe('Models', () => {
     describe('Link Station Model', () => {
-        it('Link station 1 should return correct distance and power', () => {
+        it('Link station 1 should return correct distance and power for point (0, 0)', () => {
             const myLinkStation = new LinkStation(0, 0, 10);
 
             const myPoint = new Point(0, 0);
@@ -13,7 +14,7 @@ describe('Models', () => {
             expect(myLinkStation.p).toBe(100);
        });
 
-       it('Link station 2 should return correct distance and power', () => {
+       it('Link station 2 should return correct distance and power for point (0, 0)', () => {
             const myLinkStation = new LinkStation(20, 20, 5);
 
             const myPoint = new Point(0, 0);
@@ -23,7 +24,7 @@ describe('Models', () => {
             expect(myLinkStation.p).toBe(0);
         });
 
-        it('Link station 3 should return correct distance and power', () => {
+        it('Link station 3 should return correct distance and power for point (0, 0)', () => {
             const myLinkStation = new LinkStation(10, 0, 12);
 
             const myPoint = new Point(0, 0);
@@ -35,8 +36,17 @@ describe('Models', () => {
     });
 
     describe('Link Station List Model', () => {
-        it('sth', () => {
-            expect(1).toBe(1);
+        it('A collectionn of 3 link stations should return correct distance and power', () => {
+            const myLinkStationList = new LinkStationList();
+            const myPoint = new Point(100, 100);
+            myLinkStationList.setLinkStationList(myPoint);
+            expect(myLinkStationList.linkStationList.length).toBe(3);
+            expect(myLinkStationList.linkStationList[0].d).toBe(141.4213562373095);
+            expect(myLinkStationList.linkStationList[0].p).toBe(100);
+            expect(myLinkStationList.linkStationList[1].d).toBe(113.13708498984761);
+            expect(myLinkStationList.linkStationList[1].p).toBe(0);
+            expect(myLinkStationList.linkStationList[2].d).toBe(134.5362404707371);
+            expect(myLinkStationList.linkStationList[2].p).toBe(4);
         });
     });
 });
