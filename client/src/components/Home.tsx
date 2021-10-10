9dc67b8248b3c54181465011c99207af814a1096
Khanh Dang <khanh.2dbt@gmail.com>
2021-10-10 14:45:25 +0300

    Change home text

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -3,14 +3,16 @@ import { Header } from 'semantic-ui-react';
 const Home = () => {
     return <>
         <Header as='h3' style={{ fontSize: '2em' }}>Link Station Problem</Header>
-        <p style={{ fontSize: '1.33em' }}>
-            The application is built to find the link station with the most power. The distance between the given point and the link station list is calculated in two dimensional space. The form accepts both positive and negative integers or decimals.
+        <div style={{ fontSize: '1.33em' }}>
+            <p>The application is built to find the link station with the most power. The distance between the given point and the link station list is calculated in two dimensional space. The form accepts both positive and negative integers or decimals.</p>
+            <p>3 link stations are located at point (0, 0), (20, 20) and (10, 0), in turn, with the reach value of 10, 5 and 12.</p>
+            <p>The following techs are used for the application:</p>
             <ul>
-                <li>Front-end techs: React, Typescript</li>
-                <li>Server-side techs: NodeJS, Express, Jest, Typescript</li>
+                <li>Front-end: React, Typescript</li>
+                <li>Server-side: NodeJS, Express, Jest, Typescript</li>
                 <li>Deployment: AWS</li>
             </ul>
-        </p>
+        </div>
     </>
 }
 
