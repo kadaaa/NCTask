b4f748a07e1f71217e62c46aa4495854dffdff14
Khanh Dang <khanh.2dbt@gmail.com>
2021-10-10 19:27:06 +0300

    Update calculation test

--- server/src/models/test/calculation.spec.ts
+++ server/src/models/test/calculation.spec.ts
@@ -38,14 +38,11 @@ describe('Models', () => {
     describe('Link Station List Model', () => {
         it('A collectionn of 3 link stations should return correct distance and power', () => {
             const myLinkStationList = new LinkStationList();
-            const myPoint = new Point(100, 100);
+            const myPoint = new Point(0, 0);
             myLinkStationList.setLinkStationList(myPoint);
             expect(myLinkStationList.linkStationList.length).toBe(3);
-            expect(myLinkStationList.linkStationList[0].d).toBe(141.4213562373095);
             expect(myLinkStationList.linkStationList[0].p).toBe(100);
-            expect(myLinkStationList.linkStationList[1].d).toBe(113.13708498984761);
             expect(myLinkStationList.linkStationList[1].p).toBe(0);
-            expect(myLinkStationList.linkStationList[2].d).toBe(134.5362404707371);
             expect(myLinkStationList.linkStationList[2].p).toBe(4);
         });
     });
